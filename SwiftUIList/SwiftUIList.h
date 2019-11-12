9ea76ae124c0f8ff78db5837628de06aac94ec6f
Shimaa Nagah <shimaa.nagah@code95.info>
2019-11-12 16:11:48 +0200

    Initial Commit

---
+++ SwiftUIList/SwiftUIList.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUIList.h
+//  SwiftUIList
+//
+//  Created by Shimaa Nagah on 11/12/19.
+//  Copyright © 2019 Code95. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIList.
+FOUNDATION_EXPORT double SwiftUIListVersionNumber;
+
+//! Project version string for SwiftUIList.
+FOUNDATION_EXPORT const unsigned char SwiftUIListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIList/PublicHeader.h>
+
+
